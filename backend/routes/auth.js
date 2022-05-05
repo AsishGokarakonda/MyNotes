957a644ae75b4da60d45e05b3c45d85ec2646a76
Asish <sridharnagamani5064@gmail.com>
2022-05-05 15:50:19 +0530

    jwt sent while creating user

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,9 +2,10 @@ const express = require("express");
 const usersch = require("../models/User")
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
+var bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
 
-
-
+secretKey = "nodemon@JWT"
 //create user with end point /api/auth/createuser. No login required
 router.post("/createuser",
     //validations for name,email,password
@@ -23,12 +24,20 @@ router.post("/createuser",
             if (user1) {
                 return res.status(400).json({ error: "This email already exists" })
             }
-
+            var salt = bcrypt.genSaltSync(10);
+            var hash = bcrypt.hashSync(req.body.password, salt);
+            let data ={
+                user:{
+                    id:usersch.id
+                }
+            }
+            var token = jwt.sign(data,secretKey);
+            console.log(token)
             //if user doesn't exist, new user will be created.
             let user = await usersch.create({
                 name: req.body.name,
                 email: req.body.email,
-                password: req.body.password,
+                password: hash,
             }).then(user => res.json(user))
         }
         catch (error) {
@@ -36,5 +45,8 @@ router.post("/createuser",
             res.status(500).send("Some error occured")
         }
     })
+
+
+
 
 module.exports = router
