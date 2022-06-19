f4aefcb7f2102e2243bb2b12b39320fabc7e0a85
Asish <sridharnagamani5064@gmail.com>
2022-06-19 18:01:45 +0530

    build

--- src/components/NoteItem.js
+++ src/components/NoteItem.js
@@ -9,7 +9,7 @@ export default function NoteItem(props) {
     return (
         <>
             <div className="col-md-3">
-                <div className="card" style={{ width: "18rem" }}>
+                <div className="card" style={{ width: "18rem",color:"blue" }}>
                     <div className="card-body">
                         <h5 className="card-title">{note.title}</h5>
                         <p className="card-text">{note.description}</p>
